ccb5c404ff37c9289d43abd8f6474c235964a2a7
Ansh Saini <32511936+Casey-McCray@users.noreply.github.com>
2018-08-17 15:25:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+num = input()  #Number of Transactions
+table = []
+members = 4     #Number of Members
+
+
+
+for i in range(int(num)):
+    table.append(list(map(int, input().split())))
+    
+#table = list(table)
+for i in range(int(num)):
+    construct = []
+    sum = 0
+    for j in range(0, members):
+        sum += table[i][j]
+    for j in range(0, members):
+        each = sum/members
+        table[i][j] -= each 
+    for j in range(0, members):
+        if(table[i][j] > 0):
+            string = "M{} pays M{}, M{}, M{} {} {} {} Respectively".format(j+1, 1, 2, 3, )
+        else:
+                construct.append(j)
+    print(table[i])
+    
+#print(table)
