829944baa4b554f851d9f3ab4fe846e28ec571ab
Ansh Saini <32511936+Casey-McCray@users.noreply.github.com>
2018-08-17 16:11:03 +0530

    Added Comments

--- main.py
+++ main.py
@@ -15,25 +15,25 @@ M3:
 {'M2': 200.0, 'M1': 150.0, 'M4': 150.0}
 '''
 
-num = input()   #Number of Transactions
-table = []
-members = 4     #Number of Members
+num = input()                                   #Number of Transactions
+table = []      
+members = 4                                     #Number of Members
 
-for i in range(int(num)):
+for i in range(int(num)):                       #Getting user input
     table.append(list(map(int, input().split())))
     
-for i in range(int(num)):
+for i in range(int(num)):                       #Iterating through each transaction
     dic = {}
     k = 1
     construct = []
     sum = 0
-    for j in range(0, members):
+    for j in range(0, members):                 #Loop to get the total amount for each transaction
         sum += table[i][j]
     for j in range(0, members):
-        each = sum/members
-        table[i][j] = each - table[i][j]
-        if(table[i][j] >= 0):
-            dic["M"+str(k)] = table[i][j]
+        each = sum/members                      #Calculating ideal contribution
+        table[i][j] = each - table[i][j]        
+        if(table[i][j] >= 0):                   #Loop to convert list into dictionary for easier printing
+            dic["M"+str(k)] = table[i][j]       
             k += 1
         else:
             print("M"+str(k)+": ")
